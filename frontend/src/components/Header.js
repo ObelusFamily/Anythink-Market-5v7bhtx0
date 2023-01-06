d868e4e84b2993096ba0fc1a683cc1003ed1bcb7
Tyler Thomas <66805711+TylerET@users.noreply.github.com>
2023-01-06 20:08:01 +0000

    refactor for react hooks

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -49,7 +49,7 @@ const LoggedInView = (props) => {
   );
 };
 
-const Header = () => (
+const Header = (props) => (
 <nav
         className="navbar navbar-expand-md navbar-dark"
         style={{ padding: "0.5rem 2rem" }}
@@ -58,10 +58,10 @@ const Header = () => (
           <img alt="logo" src={logo} />
         </Link>
 
-        {this.props.currentUser ? (
-          <LoggedInView currentUser={this.props.currentUser} />
+        {props.currentUser ? (
+          <LoggedInView currentUser={props.currentUser} />
         ) : (
-          <LoggedOutView currentUser={this.props.currentUser} />
+          <LoggedOutView currentUser={props.currentUser} />
         )}
       </nav>
 )
